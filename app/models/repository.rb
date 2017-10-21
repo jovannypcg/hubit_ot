f67cbad894029f71848b12a4865dc7aff93b6ef1
Jovanny Cruz <jovannypcg@yahoo.com>
2017-10-20 21:58:03 -0500

    Add parse method to user and repository models

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,10 @@
 class Repository < ApplicationRecord
   belongs_to :user
+
+  def self.parse(repo)
+    Repository.new(gh_id: repo['id'],
+        name: repo['name'],
+        description: repo['description'],
+        url: repo['html_url'])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :repositories
+
+  def self.parse(user)
+    User.new(gh_id: user['id'],
+        username: user['login'],
+        avatar: user['avatar_url'],
+        url: user['html_url'])
+  end
 end
